d8cb1bd6f4c7afba323dae2b16b3c564963806f2
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-04-02 14:41:37 +0100

    Converting to React 18

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Link, Routes, Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM, { render } from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Expenses from './routes/expenses'
@@ -7,8 +7,9 @@ import Invoices from './routes/invoices'
 import Invoice from './routes/invoice'
 
 import './index.css'
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
@@ -28,6 +29,6 @@ ReactDOM.render(
         </Route>
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
+  // document.getElementById('root')
 )
