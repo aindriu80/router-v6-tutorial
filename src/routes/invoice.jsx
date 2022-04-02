0c1682a9e68918f7bae54c48a013b679ebc9ded8
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-04-02 14:56:36 +0100

    Reading a URL Params

--- src/routes/invoice.jsx
+++ src/routes/invoice.jsx
@@ -1,10 +1,16 @@
+import { getInvoice } from '../data'
 import { useParams } from 'react-router-dom'
 function Invoice() {
   let params = useParams()
+  let invoice = getInvoice(parseInt(params.invoiceId, 10))
   return (
     <>
       <h2>The invoice page</h2>
       <h3>Invoice: {params.invoiceId}</h3>
+      <h4>
+        {invoice.name}: {invoice.number}
+      </h4>
+      <p>Date Due: {invoice.due}</p>
     </>
   )
 }
